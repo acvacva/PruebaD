c44370c35a3f045d42dd6483b9f1ba16a46d0634
sirivera <sirivera@N-5CG62309QR.nsn-intra.net>
2020-01-09 18:59:50 +0100

    segundo cambio desde eclipse

--- PruebaD/src/Controlador.java
+++ PruebaD/src/Controlador.java
@@ -4,7 +4,7 @@ public class Controlador {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("que bueno es git");
-     System.out.println("Hola Mundo");
+
 	}
 
 }
